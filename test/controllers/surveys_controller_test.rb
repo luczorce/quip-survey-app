0e97a9f682e83c4dcb3aa3bd2471f4241199cbeb
Ana Luczynski <ana.luczynski@salesforce.com>
2019-02-21 15:58:48 -0600

    add tests for the survey order

--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SurveysControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "getting all surveys in alphabetical order" do
+    get "/surveys", headers: get_test_creds()
+
+    assert_equal 200, status
+    assert_equal 4, response.parsed_body.size
+    assert_equal "A Survey which is first", response.parsed_body[0]["name"]
+    assert_equal "Zebra Survey that is last", response.parsed_body[3]["name"]
+  end
 end
